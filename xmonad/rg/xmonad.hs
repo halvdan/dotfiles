c00ffa331209f80a373be8e44d41adb4d188c550
Dan Johansson <dan.albin.johansson@gmail.com>
2015-06-03 08:38:03 +0200

    fix keyboard maps

--- xmonad/rg/xmonad.hs
+++ xmonad/rg/xmonad.hs
@@ -130,8 +130,8 @@ keys' conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- shortcuts
     , ((modm, xK_s), spawn "spotify")
-    , ((modm, xK_F9), spawn "setxkbmap -layout se")
-    , ((modm, xK_F10), spawn "setxkbmap -layout us")
+    , ((modm, xK_F9), spawn "setxkbmap -layout us")
+    , ((modm, xK_F10), spawn "setxkbmap -layout se")
 
     -- layouts
     , ((modm,               xK_space ), toggleWS' ["NSP"])
